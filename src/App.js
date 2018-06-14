19c18517b06614aa68f7539ce9c9b8df934612f7
Corbin Moore <corbin.moore27@gmail.com>
2018-06-13 18:03:46 -0600

    final push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     cards,
     counter: 0,
@@ -15,19 +14,9 @@ class App extends Component {
   };
   
   cardClick = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const cards = this.state.cards.filter(card => card.id !== id);
-    // Set this.state.friends equal to the new friends array
-   
-    // if ((this.state.guessed).length < 1) {
-    //   (this.state.guessed).push(id);
-    //   this.setState({ counter: this.state.counter + 1 });
-    //   this.highScoreCheck();
-    //   this.randomizeCards();
-    // } 
     if ((this.state.guessed).includes(id)) {
       this.setState({counter: 0})
-      this.render();
+      this.setState({guessed: []})
     } else {(this.state.guessed).push(id);
       this.setState({ counter: this.state.counter + 1 });
       this.highScoreCheck();
@@ -37,17 +26,17 @@ class App extends Component {
   };
 
   randomizeCards = cards => {
-    var shuffled = this.state.cards.sort(function() {
+    const shuffled = this.state.cards.sort(function() {
       return Math.random() - 0.5;
     })
   };
 
   highScoreCheck = () => {
     if (this.state.counter >= this.state.hScore) {
-      this.setState({hScore: this.state.counter})
+      this.setState({hScore: this.state.hScore + 1})
     }
   }
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  
   render() {
     return (
       <Wrapper>
